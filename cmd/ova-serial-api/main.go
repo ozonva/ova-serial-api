96993d11f9d49e7192422594733225a6f6d46940
anastasia611 <anastasia.rv611@gmail.com>
2021-08-07 19:45:18 +0300

    Add function for dividing slice into batches

--- cmd/ova-serial-api/main.go
+++ cmd/ova-serial-api/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("ova-serial-api")
+
+	var slice = []int{10, 20, 30, 40, 50}
+	var dividedSlice = divideSlice(slice, 2)
+
+	for _, x := range dividedSlice {
+		fmt.Print("[ ")
+		for _, y := range x {
+			fmt.Print(y)
+			fmt.Print(" ")
+		}
+		fmt.Println("]")
+	}
+}
+
+func divideSlice(slice []int, batchSize int) (newSlice [][]int) {
+	var newSize = int(math.Ceil(float64(len(slice)) / float64(batchSize)))
+	remainder := len(slice) % batchSize
+	newSlice = make([][]int, newSize)
+	for i := 0; i < newSize; i++ {
+		newBatchSize := batchSize
+		if i == newSize-1 {
+			newBatchSize = remainder
+		}
+		newSlice[i] = make([]int, newBatchSize)
+		for j := 0; j < newBatchSize; j++ {
+			newSlice[i][j] = slice[i*batchSize+j]
+		}
+	}
+	return
 }
