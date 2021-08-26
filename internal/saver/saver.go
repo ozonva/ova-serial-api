fea7ed53c1a8e7f0943b2c8d070aa8efa33233d9
anastasia611 <anastasia.rv611@gmail.com>
2021-08-26 03:08:48 +0300

    Add Saver

---
+++ internal/saver/saver.go
@@ -0,0 +1,70 @@
+package saver
+
+import (
+	"errors"
+	"ova-serial-api/internal/flusher"
+	"ova-serial-api/internal/model"
+	"sync"
+	"time"
+)
+
+type Saver interface {
+	Save(serial model.Serial) error
+	Close()
+}
+
+type saver struct {
+	capacity uint
+	flusher  flusher.Flusher
+	storage  []model.Serial
+	mutex    sync.Mutex
+}
+
+func NewSaver(
+	capacity uint,
+	flusher flusher.Flusher,
+	timeoutSec uint,
+) Saver {
+	slice := make([]model.Serial, capacity)
+
+	s := saver{
+		capacity: capacity,
+		flusher:  flusher,
+		storage:  slice,
+	}
+
+	go func() {
+		for {
+			time.Sleep(time.Duration(timeoutSec) * time.Second)
+			s.flush()
+		}
+	}()
+
+	return &s
+}
+
+func (s *saver) Save(serial model.Serial) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if len(s.storage) >= int(s.capacity) {
+		s.flush()
+
+		if len(s.storage) >= int(s.capacity) {
+			return errors.New("no capacity in storage")
+		}
+	}
+	s.storage = append(s.storage, serial)
+	return nil
+}
+
+func (s *saver) Close() {
+	s.flush()
+}
+
+func (s *saver) flush() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	s.storage = s.flusher.Flush(s.storage)
+}
